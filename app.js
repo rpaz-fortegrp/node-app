624628950cca55db1cd6ea1d67ac6262bafe4c4a
Roberto Paz <rpaz@villagemd.com>
2023-04-13 18:31:40 -0300

    Change 1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Argo CD!'))
+app.get('/', (req, res) => res.send('Hello World 1!'))
 app.listen(3000, () => console.log('Server ready'))
